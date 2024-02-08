42fca179051f557e87e8be47da23b80007659a57
proxydc <alexproxiad@gmail.com>
2024-02-08 15:52:12 +0100

    deploy

--- server/db.js
+++ server/db.js
@@ -1,11 +1,36 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'postgres',
-    user: 'postgres',
-    password: 'Proxiad2024*!'
+const pool =
+  app.get("env") == "development"
+    ? new Pool({
+        host: "localhost",
+        port: 5432,
+        database: "postgres",
+        user: "postgres",
+        password: "Proxiad2024*!",
+      })
+    : new Pool({
+        host: process.env.PG_HOST,
+        port: process.env.PG_PORT,
+        database: process.env.PG_DATABASE,
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+      });
+pool.connect((err) => {
+  if (err) {
+    console.log(
+      "connection db failed." +
+        err + "USER: "+
+        pool.user +
+        " Environment: " +
+        app.get("env")
+    );
+  } else {
+    console.log("connection db success!");
+  }
 });
 
 module.exports = pool;
